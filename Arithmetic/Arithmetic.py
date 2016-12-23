37449e4181a96100a59629c6eb9f503bc2d3a38a
Paulo R Lopez <Lopez_Paulo@outlook.com>
2016-12-22 20:27:57 -0800

    Create Arithmetic.py

---
+++ Arithmetic/Arithmetic.py
@@ -0,0 +1,55 @@
+
+"""
+
+You can call this by simply writing something like this:
+
+please note:  this is in the console only:
+
+>>> arithmetic.addition(2,4)
+>>> arithmetic.subtraction(2,4)
+>>> arithmetic.division(2,4)
+>>> arithmetic.multiplication(2,4)
+
+"""
+
+# Creating my module call arithmetic
+
+def addition(n1, n2):
+	n3 = n1 + n2  
+	print(n3)
+
+def subtraction(n1, n2):
+	n3 = n1  - n2  
+	print(n3)
+
+def division(n1, n2):
+	n3 = n1 / n2  
+	print(n3)
+
+
+def multiplication(n1,n2):
+	n3 = n1 * n2  
+	print(n3)
+
+# calling the function above 
+
+addition(2,4)
+
+subtraction(2,4)
+
+division(2,4)
+
+multiplication(2,4)
+
+# outcome 
+
+
+"""
+
+6
+-2
+0.5
+8
+
+"""
+# ===== end!
